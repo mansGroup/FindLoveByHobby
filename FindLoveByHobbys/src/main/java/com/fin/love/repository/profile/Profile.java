6e9978258b4e3d70e52889b77b56379f35ca87cf
Daehan Ji <114852681+SpaceXdaehan@users.noreply.github.com>
2023-07-31 14:02:16 +0900

    대한2 07/31
    대한2 07/31

--- FindLoveByHobbys/src/main/java/com/fin/love/repository/profile/Profile.java
+++ FindLoveByHobbys/src/main/java/com/fin/love/repository/profile/Profile.java
@@ -1,19 +1,7 @@
 package com.fin.love.repository.profile;
 
-import org.springframework.data.annotation.Id;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
 
-@Table
-@Entity
-@Builder
-@ToString
-@Getter
 public class Profile {
 
 }
