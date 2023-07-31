af9b7aeb94ce3ad86cdbae3deb85f9f7f284771d
Daehan Ji <114852681+SpaceXdaehan@users.noreply.github.com>
2023-07-31 13:48:35 +0900

    대한 07/31
    대한 07/31

--- FindLoveByHobbys/src/main/java/com/fin/love/FindLoveByHobbysApplication.java
+++ FindLoveByHobbys/src/main/java/com/fin/love/FindLoveByHobbysApplication.java
@@ -2,12 +2,14 @@ package com.fin.love;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class FindLoveByHobbysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FindLoveByHobbysApplication.class, args);
+		
 	}
-
 }
