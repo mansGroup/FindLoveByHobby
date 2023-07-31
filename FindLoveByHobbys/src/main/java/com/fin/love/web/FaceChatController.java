e3f183ef98aae51251ae0e7b4eabe86f3dbbcbb3
kim-dahun <117079872+kim-dahun@users.noreply.github.com>
2023-07-31 23:03:12 +0900

    20230731-02

--- FindLoveByHobbys/src/main/java/com/fin/love/web/FaceChatController.java
+++ FindLoveByHobbys/src/main/java/com/fin/love/web/FaceChatController.java
@@ -1,8 +1,10 @@
 package com.fin.love.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,10 +13,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FaceChatController {
 
+	// 화상채팅 방으로 입장하는 메서드
 	@GetMapping("/room")
-	public void facechatroom() {
+	public void facechatroom(Model model, @RequestParam String roomId) {
 		
-		log.info("facechatroom()");
+		model.addAttribute("roomId", roomId);
+		
+		log.info("facechatroom(roomId = {})",roomId);
+		
+	}
+	
+	// 임시 입장 메서드
+	@GetMapping("/chatroom")
+	public void chatroom() {
+		
+		log.info("chatroom");
 		
 	}
 	
--- FindLoveByHobbys/src/main/resources/static/js/facechat/facechat.js
+++ FindLoveByHobbys/src/main/resources/static/js/facechat/facechat.js
@@ -4,19 +4,31 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+	// 화면 공유를 해제하는 버튼
+	const btnScreen = document.querySelector('button#btnScreen');
+
+	// 음소거/해제 를 설정하는 버튼
+	const btnMute = document.querySelector('button#btnMute');
+
+	// 오퍼 연결을 전달하는 버튼
 	let btnOffer = document.querySelector('button#btnOffer');
+
+	// 방번호
 	let roomId = document.querySelector('input#roomId').value;
 	btnOffer.addEventListener('click', createOffer);
-	// 미디어와 관련된 변수를 선언해요 myFace는 element를 받고 myStream엔 영상 내용을 담을거에요
+
+	// MyFace = 내 카메라, peerFace = 상대 카메라, myStream = 내 카메라에서 나오는 영상 스트림
 	let myFace = document.getElementById("myFace");
 	let peerFace = document.querySelector('video#peerFace');
 	let myStream;
-	// 일단 소켓 연결을 해봐요
+
+	// wss 주소로 웹소켓을 연결(채팅에는 ws로도 가능하나, RTC는 반드시 Security 연결 필요)
 	let wss = 'wss://' + location.host + `/facechat?roomid=${roomId}`;
 
 
 	let conn = new WebSocket(wss);
-	// 먼저 RTC객체를 만들어요 구글 stun 서버로 부터 나의 정보를 가져올게요
+
+	// RTC 객체 생성 후 해당 객체에 Stun 서버 주소 전달.
 	let configuration = {
 		"iceServers": [{
 			"url": "stun:stun.l.google.com:19302"
@@ -24,103 +36,156 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 	let myPeerConnection = new RTCPeerConnection(configuration);
 
-	//미디어 내용을 받기 시작하는 함수에요
+	// mediaDevices 를 통해 영상, 음성 기기들을 찾고, 거기서 기기를 가져옴.
 	async function getMedia() {
 		try {
 			myStream = await navigator.mediaDevices.getUserMedia({
 				audio: true,
 				video: true
 			});
+
+			// 불러온 카메라를 HTML에 연결함. 이때부터 카메라에 담기는 화면이 웹에 표시됨.
 			myFace.srcObject = myStream;
 		} catch (e) {
-			console.log("미디어를 가져오는 중 에러가 발생했어요");
+			console.log("Media Link Error");
 		}
 	}
 
-	// '오퍼를 생성해요'라는 버튼을 눌렀을 때 이 메서드가 실행되요
+	// 오퍼 생성 시 해당 함수가 호출됨.
 	async function createOffer() {
-		console.log("오퍼보내기");
+		console.log("Offer Send");
 
-		//일단 카메라를 킬게요 키면서 myStream에도 미디어 정보를 담아와요
+		// 아까 작성한 카메라 연결 함수 호출.
 		await getMedia();
 
-		// getMedia에서 가져온 audio, video 트랙을 myPeerConnection에 등록해요
+		// RTC Connection에 내 화면에서 송출되는 영상, 소리를 담음.
 		await myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
 
-		// RTC객체도 만들었고 나의 미디어도 RTC객체에 담았으니 오퍼를 생성해볼게요
+		// RTC 객체 생성 -> RTC 커넥션에 트랙(영상,소리) 담기 -> Offer 전달(SDP 메시지 전달)
 		let offer = await myPeerConnection.createOffer();
 		console.log("오퍼 전달")
 		// 이제 send함수를 통해 소켓으로 나의 offer를 전송해 볼게요
-		await send({
+
+		// TODO 여기 await 지움.
+		send({
 			event: "offer",
 			data: offer
 		})
-		console.log("오퍼 전송 완료")
+		console.log("Offer send Success")
 		await myPeerConnection.setLocalDescription(offer);
 	}
 
 
 
 
-	// 내가 나의 캔디데이트(너가 나를 연결하는 방법들의 후보)를 등록하면(즉 로컬디스크립션을 설정하면)
-	// 트리거 되는 메서드에요
+	// 상대가 나를 연결할 수 있는 방법들을 만들어 전달.
 	myPeerConnection.onicecandidate = function(event) {
-		console.log("캔디데이트 전송");
+		console.log("candidate send");
 		send({
 			event: "candidate",
 			data: event.candidate
 		})
 	}
-	// 연결이 되서 피어의 스트림이 내 RTC객체에 등록되면 시작되는 메서드에요
+	// 연결이 되서 피어의 스트림이 내 RTC객체에 등록되면 자동 호출.
 	myPeerConnection.addEventListener("addstream", handleAddStream);
 	function handleAddStream(data) {
-		console.log("스트리밍 데이터 수신");
-		
+		console.log("Streaming Data Receive");
+
+		// 연결된 피어의 스트림을 웹의 상대화면 부분에 표시
 		peerFace.srcObject = data.stream;
 	}
 
-	// 소켓이 연결되었을 때 실행할 콜백함수에요
+	// 소켓 연결 시 실행할 함수
 	conn.onopen = function() {
 		console.log("웹소켓 연결 완료.");
 	};
 
-	// 소켓에서 메세지를 받아왔을 때 실행할 콜백함수에요
+	// 소켓에서 메시지 수신 시 실행할 함수
 	conn.onmessage = async function(msg) {
 		let content = JSON.parse(msg.data);
 		if (content.event == "offer") {
-			console.log("오퍼 수신");
-			// 오퍼가 오면 가장먼저 그 오퍼를 리모트 디스크립션으로 등록해줘요
+			console.log("Offer Receive");
+			// Offer가 오면 Offer를 RemoteDescription에 등록함.
 			let offer = content.data;
 			await myPeerConnection.setRemoteDescription(offer);
-			// 받는 쪽에서도 자신의 미디어를 켜줘요
+			// 수신한 쪽에도 내 카메라에서 송출되면 화면 연결.
 			await getMedia();
 			await myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
-			// 이제 앤서를 보내요
+			// answer 생성 후 LocalDescription에 전달.
 			let answer = await myPeerConnection.createAnswer();
 			await myPeerConnection.setLocalDescription(answer);
-			console.log("앤서를 보낼게요");
+			console.log("Answer Send");
 			send({
 				event: "answer",
 				data: answer
 			})
 		} else if (content.event == "answer") {
-			console.log("앤서가 왔어요");
+
+			// answer를 수신한 경우, remoteDescription에 전달.
+			console.log("Answer Receive");
 			answer = content.data;
 			await myPeerConnection.setRemoteDescription(answer);
 		} else if (content.event == "candidate") {
-			console.log("캔디데이트가 왔어요");
-			// 이 메서드를 통해 리모트 디스크립션에 설정되어있는 피어와의 연결방식을 결정해요
+			console.log("candidate Receive");
+			// RemoteDescription에 연결된 상대와 나의 연결방식을 결정.
 			await myPeerConnection.addIceCandidate(content.data);
 		}
 	}
 
-	// 앞으로 소켓으로 메세지를 보낼 땐 이 함수를 쓸 생각이에요
+	// 웹소켓 서버로 메시지 전달하는 함수
 	function send(message) {
 		conn.send(JSON.stringify(message));
 	}
-
 	
+	// 7월 31일 밤 추가 TODO
+	const mutechange = () => {
+		navigator.mediaDevices.getUserMedia({ audio: true })
+			.then(stream => {
+				// 오디오 트랙 가져오기
+				const audioTrack = stream.getAudioTracks()[0];
 
+				if (audioTrack.enabled == true) {
+					// 음소거 처리
+					console.log("mute");
+					audioTrack.enabled = false; // true로 설정하면 다시 음소거가 해제됩니다.
+					btnMute.innerHTML = '마이크 활성화';
+				} else {
+					console.log("non-mute");
+					audioTrack.enabled = true;
+					btnMute.innerHTML = '마이크 음소거';
+				}
+			})
+			.catch(error => {
+				console.error('Error accessing local media:', error);
+			});
+	}
+
+
+	// 7월 31일 밤 추가 TODO
+	btnMute.addEventListener('click', mutechange);
+
+
+	// 화면 공유 중지 버튼 클릭 이벤트 처리 ( 7월 31일 밤 추가 TODO )
+	btnScreen.addEventListener('click', async () => {
+		// 화면 공유 스트림 중지
+		if (myStream) {
+			const tracks = myStream.getTracks();
+			tracks.forEach(track => track.stop());
+			myStream = null;
+			btnScreen.innerHTML = '영상 활성화';
+		} else {
+			
+			getMedia();
+			await myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
+			btnScreen.innerHTML = '영상 숨기기';
+		}
+
+		// 화면 공유 스트림을 비디오 요소에서 해제합니다.
+		myFace.srcObject = null;
+
+		
+		
+	});
 
 })
 
