1cd48d2b3db6031904cdf42f9fe06dc065264fee
dahan <dbekgks98@gmail.com>
2023-08-08 13:19:27 +0900

    note

--- FindLoveByHobbys/src/main/java/com/fin/love/web/BaseLayoutController.java
+++ FindLoveByHobbys/src/main/java/com/fin/love/web/BaseLayoutController.java
@@ -1,4 +1,13 @@
 package com.fin.love.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BaseLayoutController {
+
+    @GetMapping("/baselayout")
+    public String baselayout() {
+        return "/layout/base_layout";
+    }
 }
---
+++ FindLoveByHobbys/src/main/java/com/fin/love/web/NoteController.java
@@ -0,0 +1,17 @@
+package com.fin.love.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequestMapping("note")
+@Controller
+public class NoteController {
+
+    @GetMapping("/note")
+    public void note() {
+        String
+    }
+}
