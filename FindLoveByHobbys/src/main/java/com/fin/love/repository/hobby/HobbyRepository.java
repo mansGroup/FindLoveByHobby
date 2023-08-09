860d3ca6bcc46ed7895a9e222eb7b5918a4694c4
Daehan Ji <114852681+SpaceXdaehan@users.noreply.github.com>
2023-08-09 14:13:00 +0900

    08/09

--- FindLoveByHobbys/src/main/java/com/fin/love/repository/hobby/HobbyRepository.java
+++ FindLoveByHobbys/src/main/java/com/fin/love/repository/hobby/HobbyRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HobbyRepository extends JpaRepository<Hobby, Long> {
 	
-	List<Hobby> findByUserid(String userid);
 	
 }
