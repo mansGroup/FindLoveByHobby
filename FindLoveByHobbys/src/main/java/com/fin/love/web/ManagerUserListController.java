fd47164adcf44cff9ccce757c2957bad0875bc1a
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2023-08-09 18:20:57 +0900

    8/09

--- FindLoveByHobbys/src/main/java/com/fin/love/web/ManagerUserListController.java
+++ FindLoveByHobbys/src/main/java/com/fin/love/web/ManagerUserListController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fin.love.dto.member.ManagerUserListDto;
@@ -25,8 +26,8 @@ public class ManagerUserListController {
 	private final ManagerUserListService mamagerUserListService;
 	private final MemberRepository memberRepository;
 	
-	@GetMapping("/user/list")
-	public String managerUserList(Model model) {
+	@GetMapping("/user/list/{pageCount}")
+	public String managerUserList(@PathVariable int pageCount, Model model) {
 		log.info("managerUserList()");
 		
 		List<ManagerUserListDto> dtos = new LinkedList<>();
@@ -40,6 +41,14 @@ public class ManagerUserListController {
 		}
 		
 		model.addAttribute("members", dtos);
+		
+		// 페이지 네이션
+		int onePageFirstCount = 1;
+		int onePageMaxCount = 5;
+		
+		model.addAttribute("onePageFirstCount", onePageFirstCount);
+		model.addAttribute("onePageMaxCount", onePageMaxCount);
+		model.addAttribute("pageCount", pageCount);
 		
 		return "/member/manager_user_list";
 	}
