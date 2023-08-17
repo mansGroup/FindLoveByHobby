eeecac109ca4c5b1eb63784666dc83faa7000ba6
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2023-08-17 15:50:50 +0900

    8/17

--- FindLoveByHobbys/src/main/java/com/fin/love/dto/profile/ProfileSearchDto.java
+++ FindLoveByHobbys/src/main/java/com/fin/love/dto/profile/ProfileSearchDto.java
@@ -1,13 +1,17 @@
 package com.fin.love.dto.profile;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ProfileSearchDto {
 	
-	private int hobby1;
-	private int hobby2;
-	private int hobby3;	
-	private int age;
-	private int height;
+	private String userId;
+	private String name;
+	private String age;
+	private String height;
+	private String introduce;
+	private String pic;
+	
 }
